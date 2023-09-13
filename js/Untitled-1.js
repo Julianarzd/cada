c6084bc90a590894cdf5f52a85171e0558b40470
Juliana Rezende <127219657+Julianarzd@users.noreply.github.com>
2023-09-13 17:18:02 -0300

    Add files via upload

---
+++ js/Untitled-1.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("#form")
+const nomeInput = document.querySelector("#nome")
+const sobrenomeInput = document.querySelector("#sobrenome")
+const emailInput = document.querySelector("#email")
+const numberInput = document.querySelector("#number")
+const passwordInput = document.querySelector("#password")       
+
+form.addEventListener()
